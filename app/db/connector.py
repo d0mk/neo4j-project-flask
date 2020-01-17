1df8d72a03c91138325b0cc177e194d4faaa2e9e
d0mk <dominik.kobyra@gmail.com>
2020-01-17 02:17:26 +0100

    connector update

--- app/db/connector.py
+++ app/db/connector.py
@@ -1,20 +1,17 @@
 import neo4j
 import json
-import os
-
-CREDENTIALS_PATH = os.path.join('C:\\', 'Users', 'Dominik', 'Desktop', 'neo4j-project')
 
 
 class Connector:
     def __init__(self):
-        uri, user, password = Connector.load_connection_parameters(CREDENTIALS_PATH)
+        uri, user, password = Connector.load_connection_parameters('app/db/data.json')
         self.driver = neo4j.GraphDatabase.driver(uri, auth=(user, password))
         self.session = self.driver.session()
 
 
     @staticmethod
     def load_connection_parameters(cred_path):
-        with open(os.path.join(cred_path, 'connection_data.json'), 'r') as f:
+        with open(cred_path, 'r') as f:
             data = json.load(f)
             return data['uri'], data['user'], data['password']
 
